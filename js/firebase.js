1403f8ef013679fa6827cb329b87df455feee741
sudojobs <shobjobs@gmail.com>
2026-01-10 01:15:44 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -2,7 +2,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-storage.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-storage.js";4
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyBERvTvOgSm5LMhZnyvLeuBbxzfp4cEnp4",
@@ -17,4 +18,5 @@ export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+
 </script>
