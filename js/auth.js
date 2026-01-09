5981fbaef83a033cb5eff3e84741c5a26236081a
sudojobs <shobjobs@gmail.com>
2026-01-09 23:37:45 +0530

    Create auth.js
    initial

---
+++ js/auth.js
@@ -0,0 +1,24 @@
+import { auth } from "./firebase.js";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut
+} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+
+export function signup(email, password) {
+  return createUserWithEmailAndPassword(auth, email, password);
+}
+
+export function login(email, password) {
+  return signInWithEmailAndPassword(auth, email, password);
+}
+
+export function googleLogin() {
+  return signInWithPopup(auth, new GoogleAuthProvider());
+}
+
+export function logout() {
+  return signOut(auth);
+}
