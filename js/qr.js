3e81335cafad4d4111dc03ab90d538b55b864714
sudojobs <shobjobs@gmail.com>
2026-01-09 23:52:16 +0530

    Update qr.js
    update-1

--- js/qr.js
+++ js/qr.js
@@ -4,3 +4,24 @@ export async function generateBoxQR(boxId) {
   const url = `${location.origin}/?box=${boxId}`;
   return await QRCode.toDataURL(url, { width: 300 });
 }
+
+import QRCode from "https://cdn.jsdelivr.net/npm/qrcode@1.5.3/+esm";
+import { getBoxes } from "./boxes.js";
+
+export async function loadQR() {
+  const view = document.getElementById("view");
+  const boxes = await getBoxes();
+
+  view.innerHTML = boxes.map(b => `
+    <div class="card">
+      <h3>${b.name}</h3>
+      <img id="qr-${b.id}">
+    </div>
+  `).join("");
+
+  boxes.forEach(async b => {
+    const url = `${location.origin}/?box=${b.id}`;
+    document.getElementById(`qr-${b.id}`).src =
+      await QRCode.toDataURL(url);
+  });
+}
