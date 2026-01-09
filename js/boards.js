ba11d90f0f0539dd657ae4418167e2fd6738096b
sudojobs <shobjobs@gmail.com>
2026-01-10 00:18:25 +0530

    Update boards.js

--- js/boards.js
+++ js/boards.js
@@ -4,6 +4,30 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
 import { auth } from "./firebase.js";
 
+import { emptyState } from "./ui.js";
+
+export async function loadBoards() {
+  const view = document.getElementById("view");
+  const boards = await getBoards();
+
+  if (!boards.length) {
+    view.innerHTML = emptyState(
+      "No Boards Added",
+      "Add development boards you own"
+    );
+    return;
+  }
+
+  view.innerHTML = boards.map(b => `
+    <div class="card">
+      <h3>${b.name}</h3>
+      <p>${b.mcu}</p>
+      <p>Qty: ${b.quantity}</p>
+    </div>
+  `).join("");
+}
+
+
 export async function addBoard(data) {
   await addDoc(collection(db, "boards"), {
     ...data,
